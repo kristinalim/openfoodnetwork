8efc63cdc67127ddbcb4b7ad34775e86fdb3e4ba
Kristina Lim <kristinalim.ph@gmail.com>
2020-02-04 18:47:27 +0800

    Revert "Wait for products panel to show in outgoing exchanges feature spec"
    This reverts commit 9d0e26ae282d6ee1f1a94571b9da09d2d9a384f5.

--- spec/features/admin/order_cycles_spec.rb
+++ spec/features/admin/order_cycles_spec.rb
@@ -370,12 +370,7 @@ feature '
         find(:css, "tags-input .tags input").set "wholesale\n"
       end
 
-      exchange_rows = page.all("table.exchanges tbody")
-      exchange_rows.each do |exchange_row|
-        exchange_row.find("td.products").click
-        # Wait for the products panel to be visible.
-        expect(exchange_row).to have_selector "tr", count: 2
-      end
+      page.all("table.exchanges tr.distributor td.products").each(&:click)
 
       uncheck "order_cycle_outgoing_exchange_2_variants_#{v1.id}"
       check "order_cycle_outgoing_exchange_2_variants_#{v2.id}"
